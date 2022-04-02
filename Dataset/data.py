68c69853576a77ad8de8a0e7625e3a1fbd2ec7c6
tazad-csci <63663893+tazad-csci@users.noreply.github.com>
2022-04-02 12:41:00 -0500

    Create data.py

---
+++ Dataset/data.py
@@ -0,0 +1,20 @@
+import requests
+import json
+
+file1 = open('captiol.csv', 'r')
+Lines = file1.readlines()
+
+for line in Lines:
+    parameters = {
+        'key': 'aBOUYZk70O7rYhUDAAnI5QCUoN8nvtip',
+        'location': line
+    }
+
+    response = requests.get(
+        "http://www.mapquestapi.com/geocoding/v1/address", params=parameters)
+    data = json.loads(response.text)['results']
+
+    lat = (data[0]['locations'][0]['latLng']['lat'])
+    lng = (data[0]['locations'][0]['latLng']['lng'])
+
+    print("%s %s %s" % (line, lat, lng))
