cfd8625e04f179aeed0f45f6fdefcc9526db0706
tazad-csci <63663893+tazad-csci@users.noreply.github.com>
2022-04-14 08:50:25 -0500

    Update data.py

--- Dataset/data.py
+++ Dataset/data.py
@@ -1,6 +1,9 @@
 import requests
 import json
 
+coordinate = []
+count = 0
+
 file1 = open('captial.csv', 'r')
 Lines = file1.readlines()
 
@@ -14,7 +17,20 @@ for line in Lines:
         "http://www.mapquestapi.com/geocoding/v1/address", params=parameters)
     data = json.loads(response.text)['results']
 
+    list = []
     lat = (data[0]['locations'][0]['latLng']['lat'])
     lng = (data[0]['locations'][0]['latLng']['lng'])
+    list.insert(0, lat)
+    list.insert(1, lng)
 
-    print("%s %s %s" % (line, lat, lng))
+    print(list)
+
+    coordinate.insert(count, list)
+    count += 1
+    list.clear()
+
+    # print("%s %s %s" % (line, lat, lng))
+
+# print("The coordinate is : ")
+# for i in coordinate:
+#     print(i, end=' ')
