47362f0381bb6b7b34f843c84cdc8a21dffb7a6f
tazad-csci <63663893+tazad-csci@users.noreply.github.com>
2022-04-14 11:14:44 -0500

    Update Graph.py

--- Graph.py
+++ Graph.py
@@ -1,3 +1,6 @@
+from sqlalchemy import null
+
+
 southwest_capitals = {'Pheonix': {'Santa Fe': [481, 'I-17, I-40, I-25'], 'Oklahoma City': [1006, "I-17, I-40"], 'Austin': [1051, "I-10, I-410, I-35"]},
                       'Santa Fe': {'Pheonix': [481, 'I-25, I-40, I-17'], 'Oklahoma City': [535, "I-40"], 'Austin': [957, "I-25, I-10, I-35"]},
                       'Oklahoma City': {'Santa Fe': [535, "I-40"], 'Pheonix': [1006, "I-40, I-17"], 'Austin': [388, "I-35"]},
@@ -16,8 +19,8 @@ midwest_capitals = {'Des Moines': {'Springfield': [337, '<I-80>, <I-74>, <I-155>
                     'Lansing': {'Springfield': [392, '<I-69>, <I-94>, <I-80>, <I-55>'], 'Indianapolis': [], 'Des Moines': [], 'St. Paul': [], 'Jefferson City': [], 'Bismarck': [], 'Lincoln': [], 'Columbus': [], 'Pierre': [], 'Madison': []},
                     'St. Paul': {'Springfield': [520, '<I-94>, <I-90>, <I-39>, <I-55>'], 'Indianapolis': [], 'Lansing': [], 'Des Moines': [], 'Jefferson City': [], 'Bismarck': [], 'Lincoln': [], 'Columbus': [], 'Pierre': [], 'Madison': []},
                     'Jefferson City': {'Springfield': [220, '<I-70>, <I-270>, <I-55>'], 'Indianapolis': [], 'Lansing': [], 'St. Paul': [], 'Des Moines': [], 'Bismarck': [], 'Lincoln': [], 'Columbus': [], 'Pierre': [], 'Madison': []},
-                    'Bismarck': {'Springfield': [], 'Indianapolis': [], 'Lansing': [], 'St. Paul': [], 'Jefferson City': [], 'Des Moines': [], 'Lincoln': [], 'Columbus': [], 'Pierre': [], 'Madison': []},
-                    'Lincoln': {'Springfield': [], 'Indianapolis': [], 'Lansing': [], 'St. Paul': [], 'Jefferson City': [], 'Bismarck': [], 'Des Moines': [], 'Columbus': [], 'Pierre': [], 'Madison': []},
+                    'Bismarck': {'Springfield': [952, '<I-94>, <I-90>, <I-39>, <I-55>'], 'Indianapolis': [], 'Lansing': [], 'St. Paul': [], 'Jefferson City': [], 'Des Moines': [], 'Lincoln': [], 'Columbus': [], 'Pierre': [], 'Madison': []},
+                    'Lincoln': {'Springfield': [530, '<I-80>, <I-74>, <I-155>, <I-55>'], 'Indianapolis': [], 'Lansing': [], 'St. Paul': [], 'Jefferson City': [], 'Bismarck': [], 'Des Moines': [], 'Columbus': [], 'Pierre': [], 'Madison': []},
                     'Columbus': {'Springfield': [], 'Indianapolis': [], 'Lansing': [], 'St. Paul': [], 'Jefferson City': [], 'Bismarck': [], 'Lincoln': [], 'Des Moines': [], 'Pierre': [], 'Madison': []},
                     'Pierre': {'Springfield': [], 'Indianapolis': [], 'Lansing': [], 'St. Paul': [], 'Jefferson City': [], 'Bismarck': [], 'Lincoln': [], 'Columbus': [], 'Des Moines': [], 'Madison': []},
                     'Madison': {'Springfield': [], 'Indianapolis': [], 'Lansing': [], 'St. Paul': [], 'Jefferson City': [], 'Bismarck': [], 'Lincoln': [], 'Columbus': [], 'Pierre': [], 'Des Moines': []},
